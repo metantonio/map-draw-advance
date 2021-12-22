d144fd6609283007dd5dcb8b8236a7837ce555c4
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-12-22 19:56:52 -0400

    update

--- main.py
+++ main.py
@@ -291,6 +291,46 @@ def menuPpal(user):
 
 def generatingMap(mapinput):
     #Distintas opciones free de visualización de mapas
+    basemaps = {
+    'Google Maps': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Maps',
+        overlay = True,
+        control = True
+    ),
+    'Google Satellite': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Satellite',
+        overlay = True,
+        control = True
+    ),
+    'Google Terrain': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Terrain',
+        overlay = False,
+        control = True
+    ),
+    'Google Satellite Hybrid': folium.TileLayer(
+        tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+        attr = 'Google',
+        name = 'Google Satellite',
+        overlay = False,
+        control = True
+    ),
+    'Esri Satellite': folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = False,
+        control = True
+    )
+    }
+    basemaps['Google Terrain'].add_to(myMap)
+    #basemaps['Google Satellite'].add_to(myMap)
+    basemaps['Esri Satellite'].add_to(myMap)
     folium.TileLayer('openstreetmap').add_to(myMap)
     folium.TileLayer('Stamen Toner').add_to(myMap)
     folium.TileLayer('Stamen Watercolor').add_to(myMap)
