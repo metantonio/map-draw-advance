fc6325af2e1542ba98c8e77019454c8321810abe
Antonio Martínez <antonio_martinez88@hotmail.com>
2021-12-23 09:07:44 -0400

    Solution to pip install geopandas

--- instalar dependencias con pip install.bat
+++ instalar dependencias con pip install.bat
@@ -33,7 +33,5 @@ pip3.9 install GDAL-3.3.3-cp39-cp39-win_amd64.whl
 pip3.9 install Fiona-1.8.20-cp39-cp39-win_amd64.whl
 pip3.9 install geopandas-0.10.2-py2.py3-none-any.whl
 
-pip install git+git://github.com/geopandas/geopandas.git
-
 echo END
 PAUSE
